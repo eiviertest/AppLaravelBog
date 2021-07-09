ee7107e24a1ecd8cef1798e13a14f5a1d9ad1743
AlejandroVargas <32745553+eiviertest@users.noreply.github.com>
2021-07-09 17:33:53 -0500

    fisrt commit

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Http\Controllers\BlogPostController;
+use App\Http\Controllers\CategoryController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', [DashboardController::class, 'index']);
+Route::get('/', 'App\Http\Controllers\DashboardController@index');
+
+Route::get('create-category', [CategoryController::class, 'create']);
+
+Route::post('post-category-form', [CategoryController::class, 'store']);
+
+Route::get('all-categories', [CategoryController::class, 'index']);
+
+Route::get('edit-category/{id}', [CategoryController::class, 'edit']);
+
+Route::post('update-category-form/{id}', [CategoryController::class, 'update']);
+
+Route::get('delete-category/{id}', [CategoryController::class, 'destroy']);
+
+Route::get('create-blog', [BlogPostController::class, 'create']);
+
+Route::post('store-blog-form', [BlogPostController::class, 'store']);
+
+//User routes
+Route::get('create-user', [UserController::class, 'create']);
+Route::get('all-users', [UserController::class, 'index']);
