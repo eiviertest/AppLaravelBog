3c8947a72e3ad75db0a68f978e8113b6c0e38d7e
AlejandroVargas <32745553+eiviertest@users.noreply.github.com>
2021-08-06 20:04:26 -0500

    Titulo correcto index celular

--- resources/views/celular/index.blade.php
+++ resources/views/celular/index.blade.php
@@ -20,7 +20,7 @@
           <div class="card mb-3">
             <div class="card-header">
               <i class="fas fa-table"></i>
-              User List</div>
+              Lista de celulares</div>
             <div class="card-body">
               <div class="table-responsive">
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
