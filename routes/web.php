d193b68fe052e90c916d1b94e4fe6a1e08d14765
AlejandroVargas <32745553+eiviertest@users.noreply.github.com>
2021-08-06 19:47:18 -0500

    Llenado de rutas para celular

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PersonaController;
+use App\Http\Controllers\CelularController;
 
 /*
 |--------------------------------------------------------------------------
@@ -43,4 +44,12 @@ Route::post('post-persona-form', [PersonaController::class, 'store']);
 Route::get('delete-persona/{id}', [PersonaController::class, 'destroy']);
 Route::get('edit-persona/{id}', [PersonaController::class, 'edit']);
 Route::post('update-persona-form/{id}', [PersonaController::class, 'update']);
+
+//Rutas para celular
+Route::get('create-celular', [CelularController::class, 'create']);
+Route::get('all-celulares', [CelularController::class, 'index']);
+Route::post('post-celular-form', [CelularController::class, 'store']);
+Route::get('delete-celular/{id}', [CelularController::class, 'destroy']);
+Route::get('edit-celular/{id}', [CelularController::class, 'edit']);
+Route::post('update-celular-form/{id}', [CelularController::class, 'update']);
 
