238e853963d6209cef8baf319749758bb415e745
AlejandroVargas <32745553+eiviertest@users.noreply.github.com>
2021-08-06 19:57:43 -0500

    Menu Celular en Sidebar

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -131,6 +131,21 @@
 
             </div>
         </li>
+
+        <!-- Menu Celular -->
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-fw fa-folder"></i>
+                <span>Celulares</span>
+            </a>
+            <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+
+                <a class="dropdown-item" href="{{ URL::to('create-celular') }}"> Registrar un celular</a>
+                <a class="dropdown-item" href="{{ URL::to('all-celular') }}"> Lsita de celulares </a>
+
+            </div>
+        </li>
+
         <li class="nav-item">
             <a class="nav-link" href="charts.html">
                 <i class="fas fa-fw fa-chart-area"></i>
